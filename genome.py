1ee08993cc981751a010ce126b10ebb2519afe33
Drake Sadosky <45882549+duufi@users.noreply.github.com>
2020-03-31 16:45:03 -0700

    Delete genome.py
    moved to jupyter

--- genome.py
+++
@@ -1,5 +0,0 @@
-'''
-Drake Sadosky - 2/24/20 - COVID work
-Mainly for the purpose of shaking off the python rust - all data will be cited - possible reports on findings of interest
-Data to be pulled from kaggle
-'''
